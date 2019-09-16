ee67cbae0a408ca843e657c9392db6647750eae0
Øyvind Raddum Berg <elacin@gmail.com>
2019-09-16 18:54:39 +0200

    WIP: Generate slinky components

---
+++ importer/src/test/resources/react-icons/check/r/react-icons/src/main/scala/typings/reactDashIcons/Slinky/Fa500px.scala
@@ -0,0 +1,21 @@
+package typings.reactDashIcons.Slinky
+
+import slinky.core.BuildingComponent
+import slinky.core.ExternalComponentWithAttributesWithRefType
+import slinky.web.html.`*`.tag
+import typings.reactDashIconDashBase.reactDashIconDashBaseMod.IconBaseProps
+import scala.scalajs.js
+import scala.scalajs.js.`|`
+import scala.scalajs.js.annotation._
+
+object Fa500px
+  extends ExternalComponentWithAttributesWithRefType[tag.type, typings.reactDashIcons.faMod.Fa500px] {
+  override val component: String | js.Object = js.constructorOf[typings.reactDashIcons.faMod.Fa500px].asInstanceOf[String | js.Object]
+  def apply(size: String | Double = null): BuildingComponent[tag.type, typings.reactDashIcons.faMod.Fa500px] = {
+    val __obj = js.Dynamic.literal()
+    if (size != null) __obj.updateDynamic("size")(size.asInstanceOf[js.Any])
+    super.apply(__obj.asInstanceOf[Props])
+  }
+  type Props = IconBaseProps
+}
+
