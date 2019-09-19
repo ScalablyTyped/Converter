80eaa2bffcd0911a3590c7cb0aa0ee5d0af88e08
Roberto Leibman <roberto@42lines.net>
2019-09-19 12:00:31 -0700

    SemanticUiReactTest

---
+++ importer/src/test/resources/semantic-ui-react/check/s/semantic-ui-react/src/main/scala/typings/semanticDashUiDashReact/inputInputMod/default.scala
@@ -0,0 +1,15 @@
+package typings.semanticDashUiDashReact.inputInputMod
+
+import scala.scalajs.js
+import scala.scalajs.js.`|`
+import scala.scalajs.js.annotation._
+
+@JSImport("semantic-ui-react/Input/Input", JSImport.Default)
+@js.native
+class default () extends Input {
+  /* CompleteClass */
+  override def focus(): Unit = js.native
+  /* CompleteClass */
+  override def select(): Unit = js.native
+}
+
