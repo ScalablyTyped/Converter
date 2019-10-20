168c0b55cb57160a3f87620dbb1a04d83e86bfd4
Øyvind Raddum Berg <elacin@gmail.com>
2019-10-21 00:54:18 +0200

    Share apply methods when possible between components
    - todo: naming

--- importer/src/test/resources/material-ui/check/m/material-ui/src/main/scala/typings/materialDashUi/ScalaJsReact/Accessibility.scala
+++ importer/src/test/resources/material-ui/check/m/material-ui/src/main/scala/typings/materialDashUi/ScalaJsReact/Accessibility.scala
@@ -1,23 +1,11 @@
 package typings.materialDashUi.ScalaJsReact
 
-import japgolly.scalajs.react.CtorType.ChildArg
-import japgolly.scalajs.react.component.JsForwardRef.UnmountedWithRoot
 import typings.materialDashUi.svgDashIconsActionAccessibilityMod.default
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 import scala.scalajs.js.annotation._
 
-object Accessibility {
-  def apply()(children: ChildArg*): UnmountedWithRoot[js.Object, default, Unit, js.Object] = {
-    val __obj = js.Dynamic.literal()
-  
-    
-  
-    val f = japgolly.scalajs.react.JsForwardRefComponent.force[
-  js.Object, 
-  japgolly.scalajs.react.Children.Varargs, 
-  typings.materialDashUi.svgDashIconsActionAccessibilityMod.default](js.constructorOf[typings.materialDashUi.svgDashIconsActionAccessibilityMod.default])
-    f(__obj.asInstanceOf[js.Object])(children: _*)
-  }
+object Accessibility extends SharedApply_Object152536094[default] {
+  override val __component: js.Any = js.constructorOf[typings.materialDashUi.svgDashIconsActionAccessibilityMod.default]
 }
 
--- importer/src/test/resources/semantic-ui-react/check/r/react/build.sbt
+++ importer/src/test/resources/semantic-ui-react/check/r/react/build.sbt
@@ -1,6 +1,6 @@
 organization := "org.scalablytyped"
 name := "react"
-version := "16.9.2-133479"
+version := "16.9.2-6678d0"
 scalaVersion := "2.12.10"
 enablePlugins(ScalaJSPlugin)
 libraryDependencies ++= Seq(
