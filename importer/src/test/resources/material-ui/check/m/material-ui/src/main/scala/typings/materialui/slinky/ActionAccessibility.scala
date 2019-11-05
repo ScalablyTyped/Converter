9564b3c2168b9aa19db735efb4a4249d5239f449
Roberto Leibman <roberto@42lines.net>
2019-11-05 15:41:52 -0800

    merging from upsteream

--- importer/src/test/resources/material-ui/check/m/material-ui/src/main/scala/typings/materialui/slinky/ActionAccessibility.scala
+++ importer/src/test/resources/material-ui/check/m/material-ui/src/main/scala/typings/materialui/slinky/ActionAccessibility.scala
@@ -1,11 +1,13 @@
-package typings.materialUi.slinky
+package typings.materialUi.Slinky
 
+import slinky.core.ExternalComponentNoPropsWithAttributesWithRefType
+import slinky.web.html.`*`.tag
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 import scala.scalajs.js.annotation._
 
 object ActionAccessibility
-  extends SharedApply_Object152536094[typings.materialUi.svgIconsActionAccessibilityMod.ActionAccessibility] {
+  extends ExternalComponentNoPropsWithAttributesWithRefType[tag.type, typings.materialUi.svgIconsActionAccessibilityMod.ActionAccessibility] {
   override val component: String | js.Object = js.constructorOf[typings.materialUi.svgIconsActionAccessibilityMod.ActionAccessibility].asInstanceOf[String | js.Object]
 }
 
--- importer/src/test/resources/semantic-ui-react/check/r/react/src/main/scala/typings/react/slinky/Fragment.scala
+++ importer/src/test/resources/semantic-ui-react/check/r/react/src/main/scala/typings/react/slinky/Fragment.scala
@@ -1,11 +1,14 @@
-package typings.react.slinky
+package typings.react.Slinky
 
+import slinky.core.ExternalComponentNoPropsWithAttributesWithRefType
+import slinky.web.html.`*`.tag
 import scala.scalajs.js
 import scala.scalajs.js.`|`
 import scala.scalajs.js.annotation._
 
 /* The following DOM/SVG props were specified: children */
-object Fragment extends SharedApply_Anon_Children298287606 {
+object Fragment
+  extends ExternalComponentNoPropsWithAttributesWithRefType[tag.type, js.Object] {
   override val component: String | js.Object = typings.react.reactMod.Fragment.asInstanceOf[String | js.Object]
 }
 
