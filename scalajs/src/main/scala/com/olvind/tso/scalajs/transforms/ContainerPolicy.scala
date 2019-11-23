4fe1aea65c6e658e653744d80ab442fed96db63c
Øyvind Raddum Berg <elacin@gmail.com>
2019-11-23 17:00:36 +0100

    Avoid objects with @JSGlobalScope with objects inside, since it breaks newer Scala.js 1.0.0-milestones

--- scalajs/src/main/scala/com/olvind/tso/scalajs/transforms/ContainerPolicy.scala
+++ scalajs/src/main/scala/com/olvind/tso/scalajs/transforms/ContainerPolicy.scala
@@ -109,7 +109,10 @@ object ContainerPolicy extends TreeTransformation {
             case _                      => false
           }
 
-        mod.members.exists {
+        def failsOnScalaJs1 =
+          mod.annotations.contains(Annotation.JsGlobalScope) && mod.members.exists(_.isInstanceOf[ModuleTree])
+
+        failsOnScalaJs1 || mod.members.exists {
           case x: InheritanceTree => check(x.annotations, x.name)
           case x: ContainerTree   => check(x.annotations, x.name)
           case x: FieldTree       => check(x.annotations, x.name)
