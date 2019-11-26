acdb2970e61cd9d22a19c1b92336f3fadb9d6510
Øyvind Raddum Berg <elacin@gmail.com>
2019-11-26 02:06:42 +0100

    A bunch of small fixes
    - handle members with names consisting of a hex number bigger than Int.MaxValue. You can't make up these edge cases >:
    - tolerate BOM in json files
    - wildcards were more trouble than I thought in react components, so eliminate them
    - cast component references for japgolly to `js.Any` to avoid some issues with components typed as `ComponentType` (a type alias to union, which doesn't comform)
    - `setCodePath` recursively for generated components so `ShortenNames` work properly afterwards.
    - Make ModuleNameParser also consider names like `@foo` non-scoped
    - handle the string literal "Empty" differently, as we already used that for (the more common case) ""

--- utils/src/main/scala/com/olvind/tso/prettyString.scala
+++ utils/src/main/scala/com/olvind/tso/prettyString.scala
@@ -94,6 +94,7 @@ object RegularPrettyString extends PrettyString {
       case "_"       => "Underscore"
       case "^"       => "`^`" // todo: think this might be solved in the printer
       case ""        => "Empty"
+      case "Empty"   => "Empty_"
       case "package" => "PACKAGE"
       case "js"      => "JS"
       case "scala"   => "SCALA"
