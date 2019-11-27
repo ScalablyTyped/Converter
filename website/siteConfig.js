790f2349705cf9b01fe1500addf4a1892e278e36
Roberto Leibman <roberto@42lines.net>
2019-11-26 21:38:43 -0800

    Putting in docosaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,27 +21,23 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'TSO', // Title for your website.
+  tagline: 'The official website of the TSO project',
+  url: 'https://scalablytyped.org', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'tso',
+  organizationName: 'scalablytyped',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    // {doc: 'doc1', label: 'Docs'},
-    // {doc: 'doc4', label: 'API'},
-    // {page: 'help', label: 'Help'},
-    // {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
