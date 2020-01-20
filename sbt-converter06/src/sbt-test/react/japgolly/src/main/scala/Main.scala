bf7d3ef3098a3ed2962f1d469b63d06447957e0c
Øyvind Raddum Berg <elacin@gmail.com>
2020-01-20 18:57:22 +0100

    Adapt scripted tests to new naming

--- sbt-converter06/src/sbt-test/react/japgolly/src/main/scala/Main.scala
+++ sbt-converter06/src/sbt-test/react/japgolly/src/main/scala/Main.scala
@@ -3,5 +3,5 @@ package simple
 import japgolly.scalajs.react.vdom.html_<^._
 object Main {
   def main(argv: Array[String]): Unit =
-    println(typingsJapgolly.react.Anon_Children("hello, world"))
+    println(typingsJapgolly.react.AnonChildren("hello, world"))
 }
--- sbt-converter06/src/sbt-test/react/slinky/src/main/scala/Main.scala
+++ sbt-converter06/src/sbt-test/react/slinky/src/main/scala/Main.scala
@@ -2,6 +2,6 @@ package simple
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(typingsSlinky.react.Anon_Children("hello, world"))
+    println(typingsSlinky.react.AnonChildren("hello, world"))
   }
 }
