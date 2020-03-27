824f5f8bb042184b06e67506b97db8d4d1d65756
Øyvind Raddum Berg <elacin@gmail.com>
2020-03-27 20:54:12 +0100

    bump deps

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,14 +4,14 @@ object Deps {
   val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.0.4"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.6.3"
-  val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.2.0"
-  val scalatest         = "org.scalatest" %% "scalatest" % "3.1.0"
-  val fansi             = "com.lihaoyi" %% "fansi" % "0.2.8"
+  val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.2.1"
+  val scalatest         = "org.scalatest" %% "scalatest" % "3.1.1"
+  val fansi             = "com.lihaoyi" %% "fansi" % "0.2.9"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.3.5"
   val bintry            = "org.foundweekends" %% "bintry" % "0.5.2"
   val asyncHttpClient   = "com.ning" % "async-http-client" % "1.9.40"
-  val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
+  val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
 
   val circe: Seq[ModuleID] =
-    Seq("core", "generic", "parser", "jackson29").map(s => "io.circe" %% s"circe-$s" % "0.11.1")
+    Seq("core", "generic", "parser", "jackson29").map(s => "io.circe" %% s"circe-$s" % "0.13.0")
 }
