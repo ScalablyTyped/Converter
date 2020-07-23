1ccf63e5d1b2b2bb81e6453252e1f16f75ef0b04
Øyvind Raddum Berg <elacin@gmail.com>
2020-07-23 23:54:12 +0200

    fix scripted tests

--- sbt-converter/src/sbt-test/react/japgolly/src/main/scala/Main.scala
+++ sbt-converter/src/sbt-test/react/japgolly/src/main/scala/Main.scala
@@ -3,5 +3,5 @@ package simple
 import japgolly.scalajs.react.vdom.html_<^._
 object Main {
   def main(argv: Array[String]): Unit =
-    println(typings.react.anon.Children("hello, world"))
+    println(typings.react.anon.Children().setChildren("hello, world"))
 }
