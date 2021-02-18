f3f34d287563b193588b8c2717ab23982ae9566e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-18 19:36:36 +0100

    Update scalatest to 3.2.4 (#252)

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.3.8"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.7.2"
   val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.2.3"
-  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.3"
+  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.4"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.2.10"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.4.5"
   val bintry            = "org.foundweekends" %% "bintry" % "0.5.2"
