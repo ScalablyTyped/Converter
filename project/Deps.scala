120395cbeba6a38d2adfb2a44b0a3ee094101872
Scala Steward <me@scala-steward.org>
2021-05-14 20:11:22 +0200

    Update scalatest to 3.2.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.3.8"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.7.6"
   val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.2.6"
-  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.8"
+  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.9"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.2.13"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.4.5"
   val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
