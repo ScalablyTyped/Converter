cd4d77b0230fe2a322ba672a43ba74ac3439ab25
Scala Steward <me@scala-steward.org>
2021-05-14 03:01:23 +0200

    Update scala-xml to 2.0.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   val scalatest         = "org.scalatest" %% "scalatest" % "3.2.8"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.2.13"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.4.5"
-  val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
+  val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "2.0.0"
   val scopt             = "com.github.scopt" %% "scopt" % "4.0.1"
   val awssdkS3          = "software.amazon.awssdk" % "s3" % "2.15.28"
   val java8Compat       = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.1"
