710ee6cad2b371bc6df13f5c20402597c17bbf65
Ingar Abrahamsen <ingar@abrahams1.com>
2021-09-17 13:55:53 +0200

    fix: scala 2 intersection type erasure

---
+++ tests/firebase/check-3/f/firebase/src/main/scala/typings/firebase/FirebaseTest.scala
@@ -0,0 +1,13 @@
+package typings.firebase
+
+import org.scalablytyped.runtime.StObject
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSGlobalScope, JSGlobal, JSImport, JSName, JSBracketAccess}
+
+@js.native
+trait FirebaseTest[T]
+  extends StObject
+     with Foo {
+  
+  def name(p: js.Any & T): Unit = js.native
+}
