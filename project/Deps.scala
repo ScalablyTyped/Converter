94665b2181330cf534781dd7334413410eded0a4
Scala Steward <me@scala-steward.org>
2021-09-29 04:51:58 +0200

    Update scala-java8-compat to 1.0.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
   val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
   val scopt             = "com.github.scopt" %% "scopt" % "4.0.1"
   val awssdkS3          = "software.amazon.awssdk" % "s3" % "2.15.28"
-  val java8Compat       = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.0"
+  val java8Compat       = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.1"
 
   val circe: Seq[ModuleID] =
     Seq(
