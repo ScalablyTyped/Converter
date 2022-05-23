93dcd8606906922b194de88ab0f388667857570d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-23 23:32:17 +0200

    Update scalatest to 3.2.12 (#442)
    Co-authored-by: Øyvind Raddum Berg <elacin@gmail.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.4.0"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.7.7"
   val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.2.8"
-  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.9"
+  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.12"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.3.1"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.4.11"
   val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
