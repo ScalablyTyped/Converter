ScalablyTyped/Converter

commit c27f72f0875c873a53a181f42adcda737051d1da

TypeParamsReferencedInTree is not precise enough, but now let it err on the safe/ugly side instead of possibly wrong.

ts/src/main/scala/org/scalablytyped/converter/internal/ts/TypeParamsReferencedInTree.scala
@@ -3,15 +3,6 @@ package ts
 
 object TypeParamsReferencedInTree {
   def apply(inScope: Map[TsIdent, TsTypeParam], tree: TsTree): IArray[TsTypeParam] = {
-
-    val locallyDefined: Set[TsIdent] =
-      TsTreeTraverse
-        .collect(tree) {
-          case HasTParams(tparams) => tparams.map(_.name)
-        }
-        .flatten
-        .toSet
-
     val referencedInTree: IArray[TsIdent] =
       TsTreeTraverse
         .collect(tree) {
@@ -29,6 +20,6 @@ object TypeParamsReferencedInTree {
           unprefixedName
       }
 
-    (referencedInTree ++ fromBounds).distinct.filterNot(locallyDefined).map(inScope)
+    (referencedInTree ++ fromBounds).distinct.map(inScope)
   }
 }

commit 7df50e75cee6df3b21629d37c399ce4dabbeb9ed

make TypeParamsReferencedInTree actually precise
fixes some fallout from c27f72f0. Pretty rare interaction between type parametrized class, static member with object type with nested type parameter with same name as outer. broke rxjs anyway

ts/src/main/scala/org/scalablytyped/converter/internal/ts/TypeParamsReferencedInTree.scala
@@ -3,13 +3,29 @@ package ts
 
 object TypeParamsReferencedInTree {
   def apply(inScope: Map[TsIdent, TsTypeParam], tree: TsTree): IArray[TsTypeParam] = {
-    val referencedInTree: IArray[TsIdent] =
-      TsTreeTraverse
-        .collect(tree) {
-          case TsTypeRef(_, TsQIdent(IArray.exactlyOne(unprefixedName)), _) if inScope.contains(unprefixedName) =>
-            unprefixedName
+    val referencedInTree: IArray[TsIdent] = {
+      val referenced = IArray.Builder.empty[TsIdent]
+
+      object Visitor extends TreeTransformation[Map[TsIdent, TsTypeParam]] {
+        override def enterTsTypeRef(inScope: Map[TsIdent, TsTypeParam])(x: TsTypeRef): TsTypeRef = {
+          x match {
+            case TsTypeRef(_, TsQIdent(IArray.exactlyOne(unprefixedName)), _) if inScope.contains(unprefixedName) =>
+              referenced += unprefixedName
+            case _ => ()
+          }
+          x
         }
-        .distinct
+
+        override def withTree(inScope: Map[TsIdent, TsTypeParam], tree: TsTree): Map[TsIdent, TsTypeParam] =
+          tree match {
+            case HasTParams(tparams) => inScope.filterNot { case (name, _) => tparams.exists(tp => name === tp.name) }
+            case _                   => inScope
+          }
+      }
+
+      Visitor.visitTsTree(inScope)(tree)
+      referenced.result().distinct
+    }
 
     /* the bound of one of the referenced tparams might refer to other tparams */
     val fromBounds: IArray[TsIdent] =