5f2397fd372590d778f695ee78254083ae2d3506
Jason Pickens <jasonpickensnz@gmail.com>
2023-04-13 09:50:50 +1200

    Fix deprecated warnings

--- sbt-converter/src/main/scala/org/scalablytyped/converter/plugin/ScalablyTypedConverterGenSourcePlugin.scala
+++ sbt-converter/src/main/scala/org/scalablytyped/converter/plugin/ScalablyTypedConverterGenSourcePlugin.scala
@@ -32,7 +32,7 @@ object ScalablyTypedConverterGenSourcePlugin extends AutoPlugin {
       stSourceGenMode := SourceGenMode.ResourceGenerator,
       Compile / sourceGenerators ++= {
         stSourceGenMode.value match {
-          case SourceGenMode.ResourceGenerator => List(stImport.taskValue)
+          case SourceGenMode.ResourceGenerator => List(stImportSources.taskValue)
           case SourceGenMode.Manual(_, _)      => Nil
         }
       },
@@ -44,7 +44,7 @@ object ScalablyTypedConverterGenSourcePlugin extends AutoPlugin {
       stMinimizeKeep := Nil,
       ScalaJsBundlerHack.adaptScalaJSBundlerPackageJson,
       ScalaJsBundlerHack.adaptNpmInstallJSResources,
-      stImport := {
+      stImportSources := {
         val stLogger   = WrapSbtLogger.task.value
         val conversion = stConversionOptions.value
 
