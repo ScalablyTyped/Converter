b812e77bc4c71ab4c38b9a2b4c06c4329d6a64dd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-11 21:56:22 +0200

    Update scalatest to 3.2.16 (#537)

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.4.0"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.7.7"
   val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.3.0"
-  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.15"
+  val scalatest         = "org.scalatest" %% "scalatest" % "3.2.16"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.4.0"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.5.4"
   val scalaXml          = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
