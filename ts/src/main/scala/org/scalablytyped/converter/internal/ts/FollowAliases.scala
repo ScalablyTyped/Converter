51dc9565ec5ad8eb39e8dce2a32b4caabe16cb50
Øyvind Raddum Berg <elacin@gmail.com>
2023-10-13 23:54:14 +0200

    FollowAliases: Avoid circular types as a consequence of interface augmentation
    ```typescript
    interface Locals extends core.Locals {}
    ```

--- ts/src/main/scala/org/scalablytyped/converter/internal/ts/FollowAliases.scala
+++ ts/src/main/scala/org/scalablytyped/converter/internal/ts/FollowAliases.scala
@@ -2,14 +2,17 @@ package org.scalablytyped.converter.internal
 package ts
 
 object FollowAliases {
-  def apply(scope: TsTreeScope, skipValidation: Boolean = false)(tpe: TsType): TsType =
+  def apply(scope: TsTreeScope, skipValidation: Boolean = false)(tpe: TsType): TsType = {
     tpe match {
       case typeRef: TsTypeRef =>
         scope
           .lookupType(typeRef.name, skipValidation)
           .collectFirst {
             case ta: TsDeclTypeAlias =>
-              apply(scope, skipValidation)(FillInTParams(ta, typeRef.tparams).alias)
+              val next = FillInTParams(ta, typeRef.tparams).alias
+              // this is a consequence of interface augmentation. see for instance `Locals` in express
+              if (next == tpe) TsTypeRef.any.copy(comments = Comments(Comment.warning(s"circular ${TsTypeFormatter(tpe)}")))
+              else apply(scope, skipValidation)(next)
             /* see through thin interfaces which might be translated into type aliases */
             case i @ TsDeclInterface(_, _, _, _, IArray.exactlyOne(_), Empty, _) =>
               apply(scope, skipValidation)(FillInTParams(i, typeRef.tparams).inheritance.head)
@@ -21,6 +24,7 @@ object FollowAliases {
         TsTypeUnion.simplified(types.map(FollowAliases(scope, skipValidation)))
       case other => other
     }
+  }
 
   def typeRef(scope: TsTreeScope)(tr: TsTypeRef): TsTypeRef =
     scope
