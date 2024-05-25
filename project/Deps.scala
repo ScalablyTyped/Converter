984aacad420cdc9aa671889d815a8107502e620d
Scala Steward <scala_steward@virtuslab.com>
2024-05-25 19:30:24 +0000

    Update sourcecode to 0.4.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ object Deps {
   val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
   val ammoniteOps       = "com.lihaoyi" %% "ammonite-ops" % "2.4.0"
   val osLib             = "com.lihaoyi" %% "os-lib" % "0.9.3"
-  val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.3.1"
+  val sourcecode        = "com.lihaoyi" %% "sourcecode" % "0.4.2"
   val scalatest         = "org.scalatest" %% "scalatest" % "3.2.17"
   val fansi             = "com.lihaoyi" %% "fansi" % "0.4.0"
   val bloop             = "ch.epfl.scala" %% "bloop-frontend" % "1.5.4"
