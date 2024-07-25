2c267a27b983051d9acd93671bb1530edee0cfa4
Jason Pickens <jasonpickensnz@gmail.com>
2024-07-26 08:48:08 +1200

    Add stripe test (#638)

--- importer/src/test/scala/org/scalablytyped/converter/internal/importer/ImporterTest.scala
+++ importer/src/test/scala/org/scalablytyped/converter/internal/importer/ImporterTest.scala
@@ -71,6 +71,7 @@ trait ImporterTest extends AnyFunSuite with ImporterHarness with ParallelTestExe
   test("recharts")(assertImportsOk("recharts", pedantic                             = true))
   test("firebase")(assertImportsOk("firebase", pedantic                             = true))
   test("prisma")(assertImportsOk("prisma", pedantic                                 = true))
+  test("stripe")(assertImportsOk("stripe", pedantic                                 = true))
 
   test("material-ui-slinky")(
     assertImportsOk("material-ui", pedantic = true, flavour = Slinky),
---
+++ tests/stripe/in/stripe-js/lib/index.d.ts
@@ -0,0 +1,14 @@
+//export * from '../dist';
+
+// import {loadStripe} from '../dist';
+// export {loadStripe};
+
+import {StripeConstructor} from "../dist/stripe-js";
+
+declare global {
+  interface Window {
+    // Stripe.js must be loaded directly from https://js.stripe.com/v3, which
+    // places a `Stripe` object on the window
+    Stripe?: StripeConstructor;
+  }
+}
