0c4c8d08ba53ef2f20316bf842c0d52633877d7e
Toshiya MORI <mori@kurusugawa.jp>
2025-03-14 05:50:26 +0900

    Enable pedantic mode for util-dynamodb import test

--- importer/src/test/scala/org/scalablytyped/converter/internal/importer/ImporterTest.scala
+++ importer/src/test/scala/org/scalablytyped/converter/internal/importer/ImporterTest.scala
@@ -72,7 +72,7 @@ trait ImporterTest extends AnyFunSuite with ImporterHarness with ParallelTestExe
   test("firebase")(assertImportsOk("firebase", pedantic                             = true))
   test("prisma")(assertImportsOk("prisma", pedantic                                 = true))
   test("node")(assertImportsOk("node", pedantic                                     = true))
-  test("util-dynamodb")(assertImportsOk("util-dynamodb", pedantic                   = false))
+  test("util-dynamodb")(assertImportsOk("util-dynamodb", pedantic                   = true))
 
   test("material-ui-slinky")(
     assertImportsOk("material-ui", pedantic = true, flavour = Slinky),
