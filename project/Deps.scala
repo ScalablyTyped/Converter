88947c2c71a512b9cafdc0195fc5f5884e46025a
Scala Steward <scala_steward@virtuslab.com>
2026-01-29 10:25:35 +0000

    Update os-lib to 0.11.7

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt.{stringToOrganization, ModuleID}
 object Deps {
   val parserCombinators   = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0"
   val ammoniteOps         = "com.lihaoyi" %% "ammonite-ops" % "2.4.1"
-  val osLib               = "com.lihaoyi" %% "os-lib" % "0.11.5"
+  val osLib               = "com.lihaoyi" %% "os-lib" % "0.11.7"
   val sourcecode          = "com.lihaoyi" %% "sourcecode" % "0.4.4"
   val scalatest           = "org.scalatest" %% "scalatest" % "3.2.19"
   val fansi               = "com.lihaoyi" %% "fansi" % "0.5.1"
